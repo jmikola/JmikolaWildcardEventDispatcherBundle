b44a3586062f1e4dd21236b28922348042d7fea1
Jeremy Mikola <jmikola@gmail.com>
2011-08-23 15:19:35 -0400

    Add tests for delayed registration of wildcard listeners

--- Tests/EventDispatcher/EventDispatcherTest.php
+++ Tests/EventDispatcher/EventDispatcherTest.php
@@ -47,23 +47,65 @@ class EventDispatcherTest extends \PHPUnit_Framework_TestCase
         $this->dispatcher->addListener($eventName, $listener, $priority);
     }
 
-    /**
-     * @dataProvider provideListenersWithWildcards
-     */
-    public function testShouldAddListenersWithWildcardsLazily2($eventName, $listener, $priority)
-    {
-        $this->innerDispatcher->expects($this->never())
-            ->method('addListener');
-
-        $this->dispatcher->addListener($eventName, $listener, $priority);
-    }
-
     public function provideListenersWithWildcards()
     {
         return array(
             array('core.*', 'callback', 0),
             array('#', array('class', 'method'), -10),
         );
+    }
+
+    public function testShouldAddListenersWithWildcardsWhenMatchingEventIsDispatched()
+    {
+        $this->innerDispatcher->expects($this->once())
+            ->id('listener-is-added')
+            ->method('addListener')
+            ->with('core.request', 'callback', 0);
+
+        $this->innerDispatcher->expects($this->once())
+            ->after('listener-is-added')
+            ->method('dispatch')
+            ->with('core.request');
+
+        $this->dispatcher->addListener('core.*', 'callback', 0);
+        $this->dispatcher->dispatch('core.request');
+    }
+
+    public function testShouldAddListenersWithWildcardsWhenListenersForMatchingEventsAreRetrieved()
+    {
+        $this->innerDispatcher->expects($this->once())
+            ->id('listener-is-added')
+            ->method('addListener')
+            ->with('core.request', 'callback', 0);
+
+        $this->innerDispatcher->expects($this->once())
+            ->after('listener-is-added')
+            ->method('getListeners')
+            ->with('core.request')
+            ->will($this->returnValue(array('callback')));
+
+        $this->dispatcher->addListener('core.*', 'callback', 0);
+
+        $this->assertEquals(array('callback'), $this->dispatcher->getListeners('core.request'));
+    }
+
+    public function testShouldNotCountWildcardListenersThatHaveNeverBeenMatchedWhenAllListenersAreRetrieved()
+    {
+        /* When getListeners() is called without an event name, it attempts to
+         * return the collection of listeners for all events it knows about.
+         * When working with wildcards, we cannot anticipate events until we
+         * encounter a matching name. Therefore, getListeners() will ignore any
+         * wildcard listeners that are registered but haven't matched anything.
+         */
+        $this->innerDispatcher->expects($this->never())
+            ->method('addListener');
+
+        $this->innerDispatcher->expects($this->any())
+            ->method('getListeners')
+            ->will($this->returnValue(array()));
+
+        $this->dispatcher->addListener('core.*', 'callback', 0);
+        $this->assertEquals(array(), $this->dispatcher->getListeners());
     }
 
     public function testAddingAndRemovingAnEventSubscriber()
