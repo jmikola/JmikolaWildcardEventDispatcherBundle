aa5a4cadc76747c9bc1358f210131ecaf1188610
Jeremy Mikola <jmikola@gmail.com>
2011-09-22 18:19:44 -0300

    Remove unnecessary use statement

--- DependencyInjection/JmikolaEventWildcardExtension.php
+++ DependencyInjection/JmikolaEventWildcardExtension.php
@@ -4,7 +4,6 @@ namespace Jmikola\EventWildcardBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
