1323224f2bf8dab0a95116e1ea45bd334cf6e386
Jeremy Mikola <jmikola@gmail.com>
2012-02-13 02:17:06 -0500

    Update test harness

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,18 @@
 <?php 
 
-if (file_exists($file = __DIR__.'/autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
-    require_once $file;
+if (file_exists($file = __DIR__.'/../vendor/.composer/autoload.php')) {
+    $loader = require_once $file;
+} else {
+    throw new RuntimeException('Install dependencies to run test suite.');
 }
+
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'Jmikola\\WildcardEventDispatcherBundle\\')) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+        return true;
+    }
+});
